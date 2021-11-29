2d6cdd10bb61e8533b12c614f785c6bc06fe38a4
makiokada <tajyccbjp@gmail.com>
2021-11-30 02:07:15 +0900

    slider追加

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("nav").hide();
+    $(".h-btn").click(function(){//メニューボタンをクリックしたとき
+        $(this).toggleClass('active');
+        // $(".h-btn img").fadeOut();
+        $("nav").toggle(300);//0.3秒で表示したり非表示にしたりする
+    });
+});
---
+++ js/slick.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(".main-visual").slick({
+        autoplay: true,
+        autoplaySpeed: 500,
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+});
