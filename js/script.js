26f92a25493859a2de855c266a8afc045e589f6f
makiokada <tajyccbjp@gmail.com>
2021-12-06 23:36:54 +0900

    edit js file

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 $(function(){
     $("nav").hide();
     $(".h-btn").click(function(){
-        // $(this).toggleClass('active');
-        // $(".h-btn img").fadeOut();
         $("nav").toggle(300);
     });
 }); 
